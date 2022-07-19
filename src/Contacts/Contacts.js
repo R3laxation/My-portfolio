da11d11eac8ec96dde18f2ad9b7414b970509e6c
R3laxation <memories@bk.ru>
2022-07-19 22:55:01 +0600

    Added everything

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -10,20 +10,20 @@ export const Contacts = () => {
     return (
         <div className={s.contactsBlock} id="Footer">
             <div className={s.container}>
-                {/*<Fade bottom>*/}
+                <Fade bottom>
                     <div className={s.contactsText}>
                         <TitleSubtitle subtitle={'Let\'s get in touch'}/>
                         <p className={s.text}>I enjoy discussing new projects and accept any challenges. Please share as much info, as
                             possible
                             so we can get the most out of our first catch-up.</p>
-                        <h3>Living in:</h3>
+                        <h3 >Living in:</h3>
                         <address>the Republic of Kazakhstan, Nur-Sultan(ex. Astana)</address>
-                        <h3>Call:</h3>
+                        <h3 >Call:</h3>
                         <p>(+060) 444 434 444</p>
                     </div>
                     <div className={s.contactsData}>
                         <TitleSubtitle subtitle={'Estimate your Project?'}/>
-                        {/*<Shake>*/}
+                        <Shake>
                             <form className={s.contactsForm}>
                                 <ContactsInput name={'What is Your Name?'}/>
                                 <ContactsInput name={'Your Email Address'}/>
@@ -33,9 +33,9 @@ export const Contacts = () => {
                                     <span></span><span></span><span></span><span></span>
                                 </button>
                             </form>
-                        {/*</Shake>*/}
+                        </Shake>
                     </div>
-                {/*</Fade>*/}
+                </Fade>
             </div>
 
 
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,6 +6,9 @@ import {TitleSubtitle} from '../Common/Components/Title/TitleSubtitle';
 import socialImage from '../Assets/images/What_is_your_social_networking_style.jpg';
 import todoImage from '../Assets/images/to-do-list-190702.jpg';
 import questionnaire  from '../Assets/images/Questionnaire--1200x900.png';
+import pizzaImage  from '../Assets/images/tasty-pepperoni-pizza-with-mushrooms-and-olives_79782-1976.jpg';
+import perfume  from '../Assets/images/Beauty-extra-still-slide-FTPZ-videoSixteenByNineJumbo1600.jpg';
+import film  from '../Assets/images/film.jpg';
 import Fade from 'react-reveal/Fade';
 
 
@@ -21,7 +24,15 @@ export const Projects = () => {
 	const quest = {
 		backgroundImage: `url(${questionnaire})`,
 	};
-
+	const pizza = {
+		backgroundImage: `url(${pizzaImage})`,
+	};
+	const fragrance = {
+		backgroundImage: `url(${perfume})`,
+	};
+	const movie = {
+		backgroundImage: `url(${film})`,
+	};
 	return (
 		<div className={s.projectsBlock} id='Projects'>
 			<Fade bottom>
@@ -30,20 +41,38 @@ export const Projects = () => {
 				<div className={s.projects}>
 					<Project buttonName={'Show'} projectTitle={'Social Network'}
 					         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem!'}
-					         style={social}
+					         style={social} link={'https://r3laxation.github.io/Social-Network/#/'}
 
 					/>
 					<Project buttonName={'Show'}
 					         projectTitle={'Todo List'}
 					         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
 					      'Ab aliquid iusto maiores maxime perspiciatis quisquam ratione sed tempore veritatis voluptatibus.'}
-					         style={todo}
+					         style={todo} link={'https://github.com/R3laxation/todoList2021'}
 					/>
 					<Project buttonName={'Show'}
 					         projectTitle={'Questionnaire'}
 					         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
 					         'Ab aliquid iusto maiores maxime perspiciatis quisquam ratione sed tempore veritatis voluptatibus.'}
 					         style={quest} link={'https://yurakholoimov.github.io/cards-front-2.0/'}
+					/>
+					<Project buttonName={'Show'}
+							 projectTitle={'React-Pizza'}
+							 description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+								 'Ab aliquid iusto maiores maxime perspiciatis quisquam ratione sed tempore veritatis voluptatibus.'}
+							 style={pizza} link={'https://r3laxation.github.io/React-Pizza/'}
+					/>
+					<Project buttonName={'Show'}
+							 projectTitle={'Fragrance Shop'}
+							 description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+								 'Ab aliquid iusto maiores maxime perspiciatis quisquam ratione sed tempore veritatis voluptatibus.'}
+							 style={fragrance} link={'https://r3laxation.github.io/Fragrance-Shop/'}
+					/>
+					<Project buttonName={'Show'}
+							 projectTitle={'Movie-search'}
+							 description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+								 'Ab aliquid iusto maiores maxime perspiciatis quisquam ratione sed tempore veritatis voluptatibus.'}
+							 style={movie} link={'https://r3laxation.github.io/Movie-Search/'}
 					/>
 				</div>
 			</div>
